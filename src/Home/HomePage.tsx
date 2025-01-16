20156b5dbc87beb689d78b73e4bb57ae3246afee
Fredy-Cruz <fredyjcr0601@gmail.com>
2025-01-16 11:26:27 -0600

    feat: add tracks component to HomePage

--- src/Home/HomePage.tsx
+++ src/Home/HomePage.tsx
@@ -1,10 +1,11 @@
-import Navbar from "../shared/components/Navbar/Navbar"
-import Tracks from "./components/Tracks"
+import Navbar from "../shared/components/Navbar/Navbar";
+import Tracks from "./components/Tracks";
+import styles from "./Home.module.css"
 
 export const HomePage = () => {
 
   return (
-    <main>
+    <main className={styles.main}>
         <Navbar/>
         <Tracks/>
     </main>
--- src/Home/components/Tracks.tsx
+++ src/Home/components/Tracks.tsx
@@ -1,21 +1,38 @@
-import { useState, useEffect } from 'react';
-import useSWR from 'swr';
-import { getSpotify } from '../../services/spotify.services';
+import { useState, useEffect } from "react";
+import useSWR from "swr";
+import { getSpotify } from "../../services/spotify.services";
+import styles from "./Tracks.module.css";
 
 const Tracks = () => {
-  const [type, setType] = useState('');
+  const [type, setType] = useState("");
 
   useEffect(() => {
-    setType('podcasts');
+    setType("tracks");
   }, []);
 
   const fetcher = () => getSpotify(type);
 
   const { data: spotify } = useSWR(`/search/?q=test&type=${type}&offset=0&limit=10&numberOfTopResults=5`, fetcher);
 
-  console.log(spotify);
-
-  return <></>;
+  return (
+    <div className={styles.container}>
+      {spotify?.tracks.items.map((track) =>(
+        <div key={track.data.id} className={styles.track}>
+          <figure className={styles.track_cover}>
+            <img src={track.data.albumOfTrack.coverArt.sources[0].url}/>
+          </figure>
+          <div className={styles.track_data}>
+            <h2>{track.data.name}</h2>
+            <label> Artist:
+              {track.data.artists.items.map((artist) =>(
+              <h3 key={artist.uri}>{artist.profile.name}</h3>
+            ))}
+            </label>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Tracks;
