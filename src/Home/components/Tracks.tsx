850bcf911843dca942a1679a10be46ed4c221bf1
Fredy-Cruz <fredyjcr0601@gmail.com>
2025-01-17 00:37:27 -0600

    feat: add more style to tracks and add a search input

--- src/Home/components/Tracks.tsx
+++ src/Home/components/Tracks.tsx
@@ -1,21 +1,30 @@
-import { useState, useEffect } from "react";
+import React, { useState } from "react";
 import useSWR from "swr";
 import { getSpotify } from "../../services/spotify.services";
 import styles from "./Tracks.module.css";
+import Loader from "../../shared/components/Loading/Loader";
 
 const Tracks = () => {
   const [query, setQuery] = useState("");
 
-  useEffect(() => {
-    setQuery("Crush 40");
-  }, []);
-
   const fetcher = () => getSpotify(query);
 
-  const { data: spotify } = useSWR(`/search/?q=${query}&type=tracks&offset=0&limit=10&numberOfTopResults=5`, fetcher);
+  const { data: spotify, isLoading } = useSWR(`/search/?q=${query}&type=tracks&offset=0&limit=10&numberOfTopResults=5`, fetcher);
+
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(e.target.value); 
+  };
 
   return (
     <div className={styles.container}>
+
+      <input type="text" value={query} onChange={handleChange} placeholder="Search..." className={styles.search}/>
+
+      {
+        isLoading && <Loader/>
+      }
+
       {spotify?.tracks.items.map((track) =>(
         <div key={track.data.id} className={styles.track}>
           <figure className={styles.track_cover}>
@@ -28,8 +37,7 @@ const Tracks = () => {
                 {track.data.artists.items.map((artist) =>(
                   <h3 key={artist.uri}>{artist.profile.name}</h3>
                 ))}
-              </span>
-              
+              </span>  
             </label>
           </div>
         </div>
